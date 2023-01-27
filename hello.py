5d0d1ed478b22cbb85c8509777a58c2fc20bcd9e
rogeeoh <rogeeoh@naver.com>
2023-01-27 20:31:12 +0900

    깃 사용법 추가

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
 print("Hello Python!")
 print("This is Version 2")
+
+# git: 코드를 버전관리 하는 도구. 세이브파일과 비슷한 역할.
+# git init : 깃을 사용하기 위한 명령어. 처음 한번만 싦행하면 됨
+# git add . : 변경된 파일을 세이브할 수 있도록 stage로 올려주는 명령어
+# git commit -m "메시지" : 변경된 파일을 세이브하는 명령어. -m에는 저장 메시지를 적는다.
+# git restore . : 파일을 잘못 작성했을 때 되돌린다.
+# git push origin main: 로컬에 저장한 세이브 내용을 원격 저장소 (깃허브)로 전달
+# git log : 세이브 저장 내역을 확인하는 명령어
